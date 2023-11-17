25ea47c6fe010f4ab9b1b937b0bc525cd3baec79
Adeline Ndacyayisenga <ndacya.adeline@gmail.com>
2023-11-17 11:04:14 -0800

    create first routes

--- routes.js
+++ routes.js
@@ -5,15 +5,21 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const { User } = require('./models');
+const { asyncHandler } = require('./middleware/async-handler');
 
 //ROUTES
 
 //Route that returns all properties and values of currently authenticated user with 200 code
-router.get('/users', (req, res) => {
-    //res.json();
-});
+router.get('/users', asyncHandler(async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+}));
 
 //Route that will create a new user
-router.post('/users', (req, res) => {
-    //res.status(201).json();
-});
+router.post('/users', asyncHandler(async (req, res) => {
+    res.status(201).location('/').json({
+        message: "User account successfully created"
+    }).end();
+}));
+
+module.exports = router;
